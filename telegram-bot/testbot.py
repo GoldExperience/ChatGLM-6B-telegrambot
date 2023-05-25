1dad49d2dcf8d48353929a234ddb50df77811eb8
Jian <pawnand.com@gmail.com>
2023-05-25 20:29:31 +0800

    change

--- telegram-bot/testbot.py
+++ telegram-bot/testbot.py
@@ -2,20 +2,21 @@ from transformers import AutoTokenizer, AutoModel
 import os
 import telebot
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
-model = model.eval()
+# tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
+# model = model.eval()
 # response, history = model.chat(tokenizer, "你好", history=[])
 # print(response)
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 print(BOT_TOKEN)
 
-bot = telebot.TeleBot(BOT_TOKEN)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda msg: True)
+@bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
-
+	bot.reply_to(message, message.text)
 
 bot.infinity_polling()
