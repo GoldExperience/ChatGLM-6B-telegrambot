b85fa4512e4bcbd710bdf43c8630a62fc3a35c7a
Jian <pawnand.com@gmail.com>
2023-05-25 20:22:19 +0800

    modify bot

--- telegram-bot/testbot.py
+++ telegram-bot/testbot.py
@@ -1,6 +1,20 @@
 from transformers import AutoTokenizer, AutoModel
+import os
+import telebot
+
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
 model = AutoModel.from_pretrained("THUDM/chatglm-6b-int4", trust_remote_code=True).half().cuda()
 model = model.eval()
-response, history = model.chat(tokenizer, "你好", history=[])
-print(response)
+# response, history = model.chat(tokenizer, "你好", history=[])
+# print(response)
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
