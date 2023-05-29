26294c8c7e991513c3d07a069edaf4bf32d5b6a8
Jian <pawnand.com@gmail.com>
2023-05-29 20:28:17 +0800

    modify

--- telegram-bot/Indexer/indexer_test.py
+++ telegram-bot/Indexer/indexer_test.py
@@ -9,16 +9,26 @@ def countToken(input_string,tokenizer):
     tokens = tokenizer.tokenize(input_string)
     return(len(tokens))
 
-chinese_tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-
 
 # This is a long document we can split up.
 with open('example_text.txt') as f:
     example_text = f.read()
+
+# # bert-chinese tokenizer
+# tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
 # text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=chunk_size, chunk_overlap=0)
-text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
+
+# # token_based tokenizer
+# text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
+
+# gpt2-tokenizer
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=chunk_size, chunk_overlap=0)
+
+
 texts = text_splitter.split_text(example_text)
+
+
 
 
 for text in texts:
