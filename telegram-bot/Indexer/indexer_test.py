bc4ea5f73f0f3f33f8699784bdb5c3591298c586
Jian <pawnand.com@gmail.com>
2023-05-29 21:19:11 +0800

    custom

--- telegram-bot/Indexer/indexer_test.py
+++ telegram-bot/Indexer/indexer_test.py
@@ -2,6 +2,7 @@ from transformers import GPT2TokenizerFast
 from langchain.text_splitter import CharacterTextSplitter,TokenTextSplitter,RecursiveCharacterTextSplitter
 from transformers import AutoTokenizer
 import sys
+import re
 
 chunk_size = int(sys.argv[1])
 
@@ -29,16 +30,35 @@ with open('example_text.txt') as f:
 # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=100, chunk_overlap=0)
 
 # Recursive
-text_splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = chunk_size,
-    chunk_overlap  = 20,
-    length_function = len,
-)
+# text_splitter = RecursiveCharacterTextSplitter(
+#     # Set a really small chunk size, just to show.
+#     chunk_size = chunk_size,
+#     chunk_overlap  = 20,
+#     length_function = len,
+# )
+
+def split_text(text, max_tokens=20):
+    # 将文本按照句子的标点进行拆分
+    sentences = re.split(r'[\n。，？！,.?!]', text)
+    result = []
+
+    for sentence in sentences:
+        if sentence:
+            # 删除前后的空白字符
+            sentence = sentence.strip()
+            # 计算中英文混合字符串的长度
+            token_count = len(sentence) + sum(map(lambda x: x >= '\u4e00' and x <= '\u9fa5', sentence))
+            # 将较长的句子进行再分割
+            if token_count > max_tokens:
+                result.extend(split_text(sentence[:max_tokens]) + split_text(sentence[max_tokens:], max_tokens))
+            else:
+                result.append(sentence)
+
+    return result
 
 
-texts = text_splitter.split_text(example_text)
-
+# texts = text_splitter.split_text(example_text)
+texts = split_text(example_text,max_tokens=chunk_size)
 
 
 chinese_tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
