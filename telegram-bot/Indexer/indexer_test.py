f0fc91bb01c648f4c67992bf2d5da4ebe92b8b02
Jian <pawnand.com@gmail.com>
2023-05-29 20:31:55 +0800

    mo

--- telegram-bot/Indexer/indexer_test.py
+++ telegram-bot/Indexer/indexer_test.py
@@ -15,15 +15,18 @@ with open('example_text.txt') as f:
     example_text = f.read()
 
 # bert-chinese tokenizer
-tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=chunk_size, chunk_overlap=0)
+# tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
+# text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=chunk_size, chunk_overlap=0)
 
-# # token_based tokenizer
+# token_based tokenizer
 # text_splitter = TokenTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
 
 # gpt2-tokenizer
 # tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 # text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=chunk_size, chunk_overlap=0)
+
+# tiktoken tokenizer
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=100, chunk_overlap=0)
 
 
 texts = text_splitter.split_text(example_text)
