e438a536f429686c2dc21eaec2ec895f6bebbbad
Jian <pawnand.com@gmail.com>
2023-05-29 18:58:26 +0800

    indexer

---
+++ telegram-bot/Indexer/indexer_test.py
@@ -0,0 +1,12 @@
+from transformers import GPT2TokenizerFast
+
+tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+# This is a long document we can split up.
+with open('telegram-bot/Indexer/example_text.txt') as f:
+    example_text = f.read()
+from langchain.text_splitter import CharacterTextSplitter
+text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=100, chunk_overlap=0)
+texts = text_splitter.split_text(example_text)
+
+for text in texts:
+    print(len(text))
