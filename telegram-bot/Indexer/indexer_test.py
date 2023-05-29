77964409edb4e8407846929647548f22d623d583
Jian <pawnand.com@gmail.com>
2023-05-29 19:21:08 +0800

    modify path

--- telegram-bot/Indexer/indexer_test.py
+++ telegram-bot/Indexer/indexer_test.py
@@ -2,7 +2,7 @@ from transformers import GPT2TokenizerFast
 
 tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
 # This is a long document we can split up.
-with open('telegram-bot/Indexer/example_text.txt') as f:
+with open('example_text.txt') as f:
     example_text = f.read()
 from langchain.text_splitter import CharacterTextSplitter
 text_splitter = CharacterTextSplitter.from_huggingface_tokenizer(tokenizer, chunk_size=100, chunk_overlap=0)
