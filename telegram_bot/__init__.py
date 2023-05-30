dcb286b3af9b7c9101c8ee13911990e2a8567573
Jian <pawnand.com@gmail.com>
2023-05-30 23:43:25 +0800

    modify

--- telegram_bot/__init__.py
+++ telegram_bot/__init__.py
@@ -1,4 +1,6 @@
 from text_splitter import *
 from chains.search_engine import duckduckgo
 
-duckduckgo.test()
+res = duckduckgo.search_text("where is dublin city university?")
+for r in res:
+    print(r)
--- telegram_bot/chains/search_engine/duckduckgo.py
+++ telegram_bot/chains/search_engine/duckduckgo.py
@@ -1,4 +1,10 @@
-import sys
-print("hello world")
-def test():
-    print("test from chains/search_engine")
+from duckduckgo_search import DDGS
+
+def search_text(search_text, region='wt-wt'):
+    result = []
+    with DDGS() as ddgs:
+        for r in ddgs.text(search_text, region=region, safesearch='Off', timelimit='y'):
+            result.append(r)
+    return result
+
+
