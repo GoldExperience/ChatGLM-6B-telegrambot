95423e8c3a73b6366a345382bcb43186237a3483
Jian <pawnand.com@gmail.com>
2023-05-30 22:31:59 +0800

    modify

--- telegram-bot/chains/search_engine/duckduckgo.py
+++ telegram-bot/chains/search_engine/duckduckgo.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.append("GoldExperience/ChatGLM-6B-telegrambot/telegram-bot")
-from text_splitter import *
 print("hello world")
+def test():
+    print("test from chains/search_engine")
