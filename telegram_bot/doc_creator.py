641a8f5907216d0d322864b941c62be27b4ba9b1
Jian <pawnand.com@gmail.com>
2023-05-31 23:57:23 +0800

    doc creator, not finished

--- telegram_bot/doc_creator.py
+++ telegram_bot/doc_creator.py
@@ -8,10 +8,17 @@ from langchain.vectorstores import Chroma
 
 
 OPENAI_KEY = os.environ.get('OPENAI_KEY')
+text_splitter = ChineseTextSpliter()
+
+
+def search_result_splitter(results):
+    for res in results:
+        split_result = text_splitter.split_text()
+
 
 def create_docs_by_search(search_text):
-    result = duckduckgo.search_text(search_text)
-
+    results = duckduckgo.search_text(search_text)
+    results = [f"title:{},content" for x in results]
     return result
 
 if __name__=="__main__":
