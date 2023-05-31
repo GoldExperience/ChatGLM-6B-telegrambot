1d8e96c0ab55ac63ee58c1101b9e944a224def3d
Jian <pawnand.com@gmail.com>
2023-05-31 15:41:32 +0800

    mo

--- telegram_bot/chains/search_engine/duckduckgo.py
+++ telegram_bot/chains/search_engine/duckduckgo.py
@@ -1,11 +1,15 @@
-from duckduckgo_search import DDGS
+from duckduckgo_search import DDGS,ddg
 
-def search_text(search_text, region='wt-wt'):
-    result = []
-    with DDGS() as ddgs:
-        for r in ddgs.text(search_text, region=region, safesearch='Off', timelimit='y'):
-            result.append(r)
-    return result
+def search_text(search_text):
+    results = ddg(keywords, region='wt-wt', safesearch='Off', time='y')
+    return results
+
+# def search_text(search_text, region='wt-wt'):
+#     result = []
+#     with DDGS() as ddgs:
+#         for r in ddgs.text(search_text, region=region, safesearch='Off', timelimit='y'):
+#             result.append(r)
+#     return result
 
 def search_answer(search_text):
     result = []
